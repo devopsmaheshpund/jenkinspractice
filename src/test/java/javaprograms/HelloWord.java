5fb9dcaa917ff921f7e74a3b6f5fde0d5f898e15
mahesh.pund <devops.maheshpund@gmail.com>
2024-06-28 12:44:41 +0530

    new method added - addition

--- src/test/java/javaprograms/HelloWord.java
+++ src/test/java/javaprograms/HelloWord.java
@@ -8,4 +8,10 @@ public class HelloWord {
 	public void helloWord() {
 		System.out.println("Hello Word");
 	}
+	
+	@Test
+	public void additionOfTwoNumbers() {
+		int addition=10+20;
+		System.out.println("Addition of two Numbers: "+addition);
+	}
 }
